3b3860b955edd5dded78ebc6262260c499b347a5
Vidyasagar Akhumukhi <sagar112113@gmail.com.com>
2025-07-27 14:01:09 +0100

    data fix

--- src/data.jsx
+++ src/data.jsx
@@ -116,8 +116,8 @@ import imgTours from './assets/Tours.png'
 import imgBackroads from './assets/Backroads.png'
 import imgReviewCarousel from './assets/ReviewCarousel.png'
 import imgFAQ from './assets/FAQ.png'
-import imgStore from '.assets/Store.png'
- 
+import imgStore from './assets/Store.png'
+
 export const projects = [
   {
     id: nanoid(),
